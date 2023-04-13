4c871202902e4ef78396d4de49b98ac3735673c6
Lotf Sadek <LotfSadek@Lotf.wo2ewgvbjnoe1ncyfn3weuac5b.dxbx.internal.cloudapp.net>
2023-04-13 14:42:31 +0000

    added CLI tool and Lib Detector

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"log"
 	"os"
+	"strings"
+
+	//"testing/quick"
 
 	gpt "github.com/PullRequestInc/go-gpt3"
 	"github.com/joho/godotenv"
 	openai "github.com/sashabaranov/go-openai"
+	"github.com/spf13/cobra"
 )
 
 func main() {
@@ -22,6 +27,10 @@ func main() {
 	OriginalPackageOpenAI(apiKey)
 	// Modified OpenAI Package
 	ModifiedOpenAIPackage(apiKey)
+	// Libraries Detector
+	GetLibrariesFromCode(apiKey)
+	// CLI Tool
+	CommandLineInterfaceTool(apiKey)
 }
 func OriginalPackageOpenAI(apiKey string) {
 	c := openai.NewClient(apiKey)
@@ -31,6 +40,7 @@ func OriginalPackageOpenAI(apiKey string) {
 		Model:     openai.GPT3Ada,
 		MaxTokens: 10,
 		Prompt:    "I am",
+		Echo:      true,
 	}
 	resp, err := c.CreateCompletion(ctx, req)
 	if err != nil {
@@ -38,10 +48,10 @@ func OriginalPackageOpenAI(apiKey string) {
 		return
 	}
 	fmt.Println("Original package output:")
-	fmt.Println(resp.Choices[0].Text)
+	fmt.Print(resp.Choices[0].Text)
 }
 func ModifiedOpenAIPackage(apiKey string) {
-	fmt.Println("Modified package output: ")
+	fmt.Println("\nModified package output: ")
 	ctx := context.Background()
 	client := gpt.NewClient(apiKey)
 	resp, err := client.Completion(ctx, gpt.CompletionRequest{
@@ -53,5 +63,93 @@ func ModifiedOpenAIPackage(apiKey string) {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Println(resp.Choices[0].Text)
+	fmt.Print(resp.Choices[0].Text)
 }
+func CommandLineInterfaceTool(apiKey string) {
+	fmt.Println("\nCLI Tool:")
+	log.SetOutput(new(NullWriter))
+	ctx := context.Background()
+	client := gpt.NewClient(apiKey)
+	rootCmd := &cobra.Command{
+		Use:   "chatgpt",
+		Short: "Chat with ChatGPT in console.",
+		Run: func(cmd *cobra.Command, args []string) {
+			scanner := bufio.NewScanner(os.Stdin)
+			quit := false
+
+			for !quit {
+				fmt.Println("Say something ('quit' to end):")
+				if !scanner.Scan() {
+					break
+				}
+				question := scanner.Text()
+				switch question {
+				case "quit":
+					quit = true
+				default:
+					GetResponse(ctx, client, question)
+				}
+			}
+		},
+	}
+	rootCmd.Execute()
+}
+func GetResponse(ctx context.Context, client gpt.Client, question string) {
+	err := client.CompletionStreamWithEngine(ctx, gpt.TextDavinci003Engine, gpt.CompletionRequest{
+		Prompt: []string{
+			question,
+		},
+		MaxTokens:   gpt.IntPtr(512),
+		Temperature: gpt.Float32Ptr(0),
+	}, func(resp *gpt.CompletionResponse) {
+		fmt.Print(resp.Choices[0].Text)
+	})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(13)
+	}
+	fmt.Printf("\n")
+	fmt.Printf("\n")
+
+}
+func GetLibrariesFromCode(apiKey string) {
+	fmt.Println("\nLibraries Detector Tool:")
+	log.SetOutput(new(NullWriter))
+	ctx := context.Background()
+	client := gpt.NewClient(apiKey)
+
+	const inputFile = "./input_with_code.txt"
+	fileBytes, err := os.ReadFile(inputFile)
+	if err != nil {
+		log.Fatalf("failed to read file: %v", err)
+	}
+	// gpt prompt - backticks to indicate a code snippet for gpt interpreter
+	msgPrefix := "give me a shortlist of the libraries that are used in the code \n``` python\n"
+	msgSuffix := "\n```"
+	msg := msgPrefix + string(fileBytes) + msgSuffix
+
+	outputBuilder := strings.Builder{}
+
+	err = client.CompletionStreamWithEngine(ctx, gpt.TextDavinci003Engine, gpt.CompletionRequest{
+		Prompt: []string{
+			msg,
+		},
+		MaxTokens:   gpt.IntPtr(3000),
+		Temperature: gpt.Float32Ptr(0),
+	}, func(resp *gpt.CompletionResponse) {
+		outputBuilder.WriteString(resp.Choices[0].Text)
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	output := strings.TrimSpace(outputBuilder.String())
+	const outputFile = "./output.txt"
+	err = os.WriteFile(outputFile, []byte(output), os.ModePerm)
+	if err != nil {
+		log.Fatalf("Failed to write file: %v", err)
+	}
+}
+
+type NullWriter int
+
+func (NullWriter) Write([]byte) (int, error) { return 0, nil }
