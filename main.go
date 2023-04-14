45987b5169dc56aad53b9a8221c7362657ab6766
Lotf Sadek <LotfSadek@Lotf.wo2ewgvbjnoe1ncyfn3weuac5b.dxbx.internal.cloudapp.net>
2023-04-14 13:04:28 +0000

    with switch

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"context"
+	"encoding/base64"
 	"fmt"
+	"image/png"
 	"log"
 	"os"
 	"strings"
-
-	//"testing/quick"
 
 	gpt "github.com/PullRequestInc/go-gpt3"
 	"github.com/joho/godotenv"
@@ -17,20 +18,33 @@ import (
 )
 
 func main() {
+	var choice int
 	godotenv.Load()
 	apiKey := os.Getenv("API_KEY")
 	if apiKey == "" {
 		fmt.Println("no key")
 		os.Exit(1)
 	}
-	// OpenAI Package
-	OriginalPackageOpenAI(apiKey)
-	// Modified OpenAI Package
-	ModifiedOpenAIPackage(apiKey)
-	// Libraries Detector
-	GetLibrariesFromCode(apiKey)
-	// CLI Tool
-	CommandLineInterfaceTool(apiKey)
+	fmt.Println("Please choose from the options below (or 0 to quit):\n1) Text Completion\n2) CLI ChatGPT Tool\n3) Identify Libraries In the Code Provided\n4) Generate An Image Using DALL-E")
+	fmt.Scanln(&choice)
+
+	switch choice {
+	case 1:
+		ModifiedOpenAIPackage(apiKey)
+	case 2:
+		CommandLineInterfaceTool(apiKey)
+	case 3:
+		GetLibrariesFromCode(apiKey)
+	case 4:
+		ImageCreatorDallE(apiKey)
+	case 0:
+		fmt.Println("Thank you!")
+		os.Exit(1)
+	default:
+		fmt.Println("Please choose from the options below:\n1) Text Completion\n2)CLI ChatGPT Tool\n3)Identify Libraries In the Code Provided\n4) Generate An Image Using DALL-E")
+		fmt.Scanln(&choice)
+	}
+
 }
 func OriginalPackageOpenAI(apiKey string) {
 	c := openai.NewClient(apiKey)
@@ -52,18 +66,19 @@ func OriginalPackageOpenAI(apiKey string) {
 }
 func ModifiedOpenAIPackage(apiKey string) {
 	fmt.Println("\nModified package output: ")
+	fmt.Printf("\nPrompt to be completed is: %s", "computers are made of")
 	ctx := context.Background()
 	client := gpt.NewClient(apiKey)
 	resp, err := client.Completion(ctx, gpt.CompletionRequest{
-		Prompt:    []string{"the one thing you need to know about golang programming language is"},
-		MaxTokens: gpt.IntPtr(50),
+		Prompt:    []string{"computers are made of"},
+		MaxTokens: gpt.IntPtr(512),
 		Stop:      []string{"."},
 		Echo:      true,
 	})
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Print(resp.Choices[0].Text)
+	fmt.Println("\n", resp.Choices[0].Text)
 }
 func CommandLineInterfaceTool(apiKey string) {
 	fmt.Println("\nCLI Tool:")
@@ -150,6 +165,78 @@ func GetLibrariesFromCode(apiKey string) {
 	}
 }
 
+func ImageCreatorDallE(apiKey string) {
+	c := openai.NewClient(apiKey)
+	ctx := context.Background()
+	var prompt string
+	fmt.Println("Enter a textual description to generate an image from: ")
+	fmt.Scanln(&prompt)
+	// Sample image by link
+	reqUrl := openai.ImageRequest{
+		Prompt:         prompt,
+		Size:           openai.CreateImageSize256x256,
+		ResponseFormat: openai.CreateImageResponseFormatURL,
+		N:              1,
+	}
+
+	respUrl, err := c.CreateImage(ctx, reqUrl)
+	if err != nil {
+		fmt.Printf("Image creation error: %v\n", err)
+		return
+	}
+	fmt.Println("Image URL:")
+	fmt.Println(respUrl.Data[0].URL)
+
+	// Example image as base64
+	reqBase64 := openai.ImageRequest{
+		Prompt:         "Michael Jordan dunking a football",
+		Size:           openai.CreateImageSize256x256,
+		ResponseFormat: openai.CreateImageResponseFormatB64JSON,
+		N:              1,
+	}
+
+	respBase64, err := c.CreateImage(ctx, reqBase64)
+	if err != nil {
+		fmt.Printf("Image creation error: %v\n", err)
+		return
+	}
+
+	imgBytes, err := base64.StdEncoding.DecodeString(respBase64.Data[0].B64JSON)
+	if err != nil {
+		fmt.Printf("Base64 decode error: %v\n", err)
+		return
+	}
+
+	r := bytes.NewReader(imgBytes)
+	imgData, err := png.Decode(r)
+	if err != nil {
+		fmt.Printf("PNG decode error: %v\n", err)
+		return
+	}
+
+	file, err := os.Create("example.png")
+	if err != nil {
+		fmt.Printf("File creation error: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	if err := png.Encode(file, imgData); err != nil {
+		fmt.Printf("PNG encode error: %v\n", err)
+		return
+	}
+
+	fmt.Println("The image was saved as example.png")
+}
+
 type NullWriter int
 
 func (NullWriter) Write([]byte) (int, error) { return 0, nil }
+
+type ImagePrompt struct {
+	Text string `json:"text"`
+}
+
+type ImageResponse struct {
+	Image string `json:"image"`
+}
